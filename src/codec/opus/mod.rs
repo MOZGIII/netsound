4cb97be75bcdf301d4a7786fc85913f8536d70bc
MOZGIII <mike-n@narod.ru>
2019-09-16 14:47:43 +0300

    Fixed opus buffer size determination logic

--- src/codec/opus/mod.rs
+++ src/codec/opus/mod.rs
@@ -58,29 +58,35 @@ pub fn buf_size(
 ) -> usize {
     // TODO: use smaller buffer size when possible.
     // See https://tools.ietf.org/html/rfc6716#section-2
-    MAX_FRAME_SIZE as usize
+    MAX_FRAME_SIZE_PER_CHANNEL as usize * channels as usize
 }
 
 /// Takes Sample Rate in kHz and Frame Size in ms and returns frame size per
 /// channel in bytes.
+#[allow(dead_code)]
 pub const fn frame_size_per_channel(sample_rate_khz: u32, frame_size_ms: u32) -> usize {
     (sample_rate_khz as usize) * (frame_size_ms as usize)
 }
 
 /// Opus codec allows up to 120 ms frames.
+#[allow(dead_code)]
 pub const MAX_FRAME_SIZE_MS: u32 = 120;
 
 /// Maximum sample rate supportws by opus is 48 kHz.
+#[allow(dead_code)]
 pub const MAX_SAMPLE_RATE_KHZ: u32 = 48;
 
 /// Maximum sample count per channel is 48 kHz * maximum frame size in
 /// milliseconds.
+#[allow(dead_code)]
 pub const MAX_FRAME_SIZE_PER_CHANNEL: usize =
     frame_size_per_channel(MAX_SAMPLE_RATE_KHZ, MAX_FRAME_SIZE_MS);
 
-// Maximum channels supported by opus.
+/// Maximum channels supported by opus.
+#[allow(dead_code)]
 pub const MAX_CHANNELS: u32 = 2;
 
 /// Maximum sample count per frame is 48 kHz * maximum frame size in
 /// milliseconds * maximum number of channels.
+#[allow(dead_code)]
 pub const MAX_FRAME_SIZE: usize = MAX_FRAME_SIZE_PER_CHANNEL * (MAX_CHANNELS as usize);
