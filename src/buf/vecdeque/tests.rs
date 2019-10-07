2dfb5f48a6c0765bb452bf237d0f1c7272dcd959
MOZGIII <mike-n@narod.ru>
2019-10-07 18:55:18 +0300

    More vecdeque tests

--- src/buf/vecdeque/tests.rs
+++ src/buf/vecdeque/tests.rs
@@ -1,9 +1,10 @@
 use super::*;
 use crate::io::*;
 use futures::executor::block_on;
+use futures::future::FutureExt;
 use futures::pin_mut;
-use futures::task::Poll;
-use futures_test::task::panic_context;
+use futures::task::{Context, Poll};
+use futures_test::task::{new_count_waker, panic_context};
 use std::thread;
 use std::time::Duration;
 
@@ -157,4 +158,98 @@ fn test_wakers() {
 
     let items_read = block_on(reader.read_items(&mut read_buf[..])).unwrap();
     assert_eq!(items_read, 2);
+}
+
+#[test]
+fn test_poll_wakers_read() {
+    // Ensure VecDeque has expected capacity.
+    let vd = VecDeque::from(vec![1, 2, 3]);
+    assert_eq!(vd.capacity(), 3);
+
+    let (mut writer, mut reader) = vec_deque_buffer(vd);
+
+    let mut read_buf = [0u8; 1024];
+
+    let (waker, count) = new_count_waker();
+    let mut cx = Context::from_waker(&waker);
+    assert_eq!(count, 0);
+
+    let items_read_poll = reader.read_items(&mut read_buf[..]).poll_unpin(&mut cx);
+    assert!(items_read_poll.is_ready());
+    assert_eq!(count, 0);
+    let items_read = assert_extract_ready(items_read_poll).unwrap();
+    assert_eq!(items_read, 3);
+
+    let items_read_poll = reader.read_items(&mut read_buf[..]).poll_unpin(&mut cx);
+    assert!(items_read_poll.is_pending());
+    assert_eq!(count, 0);
+
+    let items_written_poll = writer.write_items(&[4, 5]).poll_unpin(&mut cx);
+    assert!(items_written_poll.is_ready());
+    assert_eq!(count, 1);
+    let items_written = assert_extract_ready(items_written_poll).unwrap();
+    assert_eq!(items_written, 2);
+
+    let items_read_poll = reader.read_items(&mut read_buf[..]).poll_unpin(&mut cx);
+    assert!(items_read_poll.is_ready());
+    assert_eq!(count, 1);
+    let items_read = assert_extract_ready(items_read_poll).unwrap();
+    assert_eq!(items_read, 2);
+}
+
+#[test]
+fn test_poll_wakers_write() {
+    // Ensure VecDeque has expected capacity.
+    let vd = VecDeque::with_capacity(3);
+    assert_eq!(vd.capacity(), 3);
+
+    let (mut writer, mut reader) = vec_deque_buffer(vd);
+
+    let mut read_buf = [0u8; 1024];
+
+    let (waker, count) = new_count_waker();
+    let mut cx = Context::from_waker(&waker);
+    assert_eq!(count, 0);
+
+    let items_written_poll = writer.write_items(&[1, 2]).poll_unpin(&mut cx);
+    assert!(items_written_poll.is_ready());
+    assert_eq!(count, 0);
+    let items_written = assert_extract_ready(items_written_poll).unwrap();
+    assert_eq!(items_written, 2);
+
+    let items_written_poll = writer.write_items(&[3, 4]).poll_unpin(&mut cx);
+    assert!(items_written_poll.is_ready());
+    assert_eq!(count, 0);
+    let items_written = assert_extract_ready(items_written_poll).unwrap();
+    assert_eq!(items_written, 1);
+
+    let items_written_poll = writer.write_items(&[5, 6]).poll_unpin(&mut cx);
+    assert!(items_written_poll.is_pending());
+    assert_eq!(count, 0);
+
+    let items_read_poll = reader.read_items(&mut read_buf[..]).poll_unpin(&mut cx);
+    assert!(items_read_poll.is_ready());
+    assert_eq!(count, 1);
+    let items_read = assert_extract_ready(items_read_poll).unwrap();
+    assert_eq!(items_read, 3);
+
+    let items_written_poll = writer.write_items(&[5, 6]).poll_unpin(&mut cx);
+    assert!(items_written_poll.is_ready());
+    assert_eq!(count, 1);
+    let items_written = assert_extract_ready(items_written_poll).unwrap();
+    assert_eq!(items_written, 2);
+
+    let items_read_poll = reader.read_items(&mut read_buf[..]).poll_unpin(&mut cx);
+    assert!(items_read_poll.is_ready());
+    assert_eq!(count, 1);
+    let items_read = assert_extract_ready(items_read_poll).unwrap();
+    assert_eq!(items_read, 2);
+}
+
+fn assert_extract_ready<T>(poll: Poll<T>) -> T {
+    assert!(poll.is_ready());
+    match poll {
+        Poll::Ready(val) => val,
+        Poll::Pending => unreachable!(),
+    }
 }
