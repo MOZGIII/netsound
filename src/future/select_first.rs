3ddc8e3ed7a91f69baeae9a8b00742c956465cdf
MOZGIII <mike-n@narod.ru>
2019-10-10 05:32:31 +0300

    Exposed take_first from future::select_first

--- src/future/select_first.rs
+++ src/future/select_first.rs
@@ -6,7 +6,11 @@ where
     A: Future<Output = T> + Unpin,
     B: Future<Output = T> + Unpin,
 {
-    match select(future1, future2).await {
+    take_first(select(future1, future2).await)
+}
+
+pub fn take_first<T, A, B>(selected: Either<(T, B), (T, A)>) -> T {
+    match selected {
         Either::Left((x, _)) => x,
         Either::Right((x, _)) => x,
     }
